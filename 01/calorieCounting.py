6dd63ce7f2a62aa9623ef94b3b4ad0b52026536c
Clément Bellot <clement.bellot@ens-lyon.fr>
2022-12-01 11:40:50 +0100

    add day 01

--- 01/calorieCounting.py
+++ 01/calorieCounting.py
@@ -2,8 +2,16 @@ import sys
 
 def partOne(inpu) :
     with open(inpu,'r') as inp :
-        pass
-    return
+        elv=[]
+        s=0
+        for i in inp :
+            if i!="\n":
+                s+=int(i[:-1])
+            else :
+                elv.append(s)
+                s=0
+        elv.append(s)
+    return max(elv)
 
 print()
 print("==> PART ONE <==")
@@ -12,8 +20,17 @@ print()
 
 def partTwo(inpu) :
     with open(inpu,'r') as inp :
-        pass
-    return
+        elv=[]
+        s=0
+        for i in inp :
+            if i!="\n":
+                s+=int(i[:-1])
+            else :
+                elv.append(s)
+                s=0
+        elv.append(s)
+        elv.sort(reverse=True)
+    return sum(elv[0:3])
 
 print("==> PART TWO <==")
 print(partTwo(sys.argv[1]))
