76dfdc5a6e463ba267a2cf60dccdfe4eb71ec422
Clément Bellot <clement.bellot@ens-lyon.fr>
2022-12-01 11:30:25 +0100

    initial commit

---
+++ 01/calorieCounting.py
@@ -0,0 +1,21 @@
+import sys
+
+def partOne(inpu) :
+    with open(inpu,'r') as inp :
+        pass
+    return
+
+print()
+print("==> PART ONE <==")
+print(partOne(sys.argv[1]))
+print()
+
+def partTwo(inpu) :
+    with open(inpu,'r') as inp :
+        pass
+    return
+
+print("==> PART TWO <==")
+print(partTwo(sys.argv[1]))
+
+
